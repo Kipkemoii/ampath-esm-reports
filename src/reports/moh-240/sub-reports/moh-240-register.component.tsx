e4a59ddcb0ae1198d46e76862c932bafb32f6f3a
Walter Kiprono <walterkiprono81@gmail.com>
2026-05-07 15:06:58 +0300

    fix: add key to moh-240 datatable

--- src/reports/moh-240/sub-reports/moh-240-register.component.tsx
+++ src/reports/moh-240/sub-reports/moh-240-register.component.tsx
@@ -52,9 +52,9 @@ const Moh240Register: React.FC<Moh240RegisterProps> = ({ patientList = [], indic
 
     const resultField = getResultField(indicator);
 
-    const subHeaders = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z', 'AA'];
+    const subHeaders = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA'];
 
-    const rows = patientList.map(patient => {
+    const rows = patientList.map((patient, index) => {
         const row: any = {};
         headers.forEach(header => {
             if (header.key === 'results') {
@@ -62,11 +62,12 @@ const Moh240Register: React.FC<Moh240RegisterProps> = ({ patientList = [], indic
             } else {
                 row[header.key] = patient[header.key] || '';
             }
+            row["id"] = header.key + index;
         });
         return row;
     });
 
-    return <DatatableWrapper headers={headers} rows={rows} subHeaders={subHeaders}/>
+    return <DatatableWrapper headers={headers} rows={rows} subHeaders={subHeaders} />
 }
 
 export default Moh240Register;
